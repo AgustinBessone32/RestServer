17ca874d6369c3d163e5c69550aaf6cd0aa9d0a5
Agustin Bessone <bessone98@gmail.com>
2022-05-09 15:46:36 -0300

    finalizada etapa de inicio con google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const User = require('../models/usuario')
 const bcrypt = require('bcryptjs');
 const usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generar-jwt");
+const {googleVerify} = require('../helpers/google-verify')
+
 
 
 const login = async(req, res = response) => {
@@ -53,6 +55,51 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async(req,res=response) => {
+
+    const {id_token} = req.body
+
+    try {
+        const {name,email,picture} = await googleVerify(id_token)
+        
+        let user = await User.findOne({email})
+
+        if(!user){
+            const data = {
+                name,
+                email,
+                password:':P',
+                img: picture,
+                google: true,
+                role: 'USER'
+            }
+
+            user = new User(data)
+            await user.save()
+        }
+
+        if(!user.status ){
+            res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        const token = await generarJWT(user.id)
+
+        res.json({
+            user,
+            token
+        })
+        
+    } catch (error) {
+         res.status(400).json({
+             msg: ' El token no se pudo verificar'
+         })
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
